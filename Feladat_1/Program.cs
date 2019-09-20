d8e64142492ca02b22fe4f5172b91de3a43397d5
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 16:08:14 +0200

    10 szam beker

--- Feladat_1/Program.cs
+++ Feladat_1/Program.cs
@@ -8,8 +8,22 @@ namespace Feladat_1
 {
     class Program
     {
+    public static List<int> lista;
+
         static void Main(string[] args)
         {
+            lista = new List<int>();
+            int szam;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Kérek adjon meg egy számot!");
+                szam = Convert.ToInt32(Console.ReadLine());
+                lista.Add(szam);
+            }
+            int max= lista.Max();
+            Console.WriteLine("A legnagyobb elem a listában."+max);
+            Console.ReadKey();
+
         }
     }
 }
